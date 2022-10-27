22972d7711bab87f5ac6a4a5976463791b2c8dd0
adnanAhmad99 <0adnanahmad99@gmail.com>
2022-10-27 09:22:37 +0500

    added basic functionalities to certificate page

--- src/Components/FoormComponents/formPages/FormCertificationPage.jsx
+++ src/Components/FoormComponents/formPages/FormCertificationPage.jsx
@@ -1,30 +1,44 @@
 import React, { useState } from "react";
 
 export default function FormCertificationPage() {
+  const initialData = {
+    subjectName: "",
+    subjectNameError: "",
+    certificateName: "",
+    certificateNameError: "",
+    certificateDescription: "",
+    certificateDescriptionError: "",
+    certificateIssuer: "",
+    certificateIssuerError: "",
+    startStudyYear: "",
+    endStudyYear: "",
+    studyYearError: "",
+  };
   const [teachingCertificatesArray, setteachingCertificatesArray] = useState([
-    {
-      subjectName: "",
-      subjectNameError: "",
-      certificateName: "",
-      certificateNameError: "",
-      certificateDescription: "",
-      certificateDescriptionError: "",
-      certificateIssuer: "",
-      certificateIssuerError: "",
-      startStudyYear: "",
-      endStudyYear: "",
-      studyYearError: "",
-    },
+    initialData,
   ]);
 
   // functions
   const handleNewDataChanges = (e, indexposition) => {
-    console.log(e.target);
-    console.log(indexposition);
+    const { name, value } = e.target;
+
+    const newDataArray = [...teachingCertificatesArray];
+    const newDataEntered = { ...newDataArray[indexposition], [name]: value };
+
+    newDataArray[indexposition] = newDataEntered;
+
+    setteachingCertificatesArray(newDataArray);
   };
   const handleImageFileUploadData = (e, indexposition) => {
     console.log(e.target);
     console.log(indexposition);
+  };
+  const handleCertificationDeleltion = (indexPosition) => {
+    // console.log(indexPosition);
+    const newDataArray = teachingCertificatesArray.filter(
+      (element, index) => index != indexPosition
+    );
+    setteachingCertificatesArray(newDataArray);
   };
 
   const handleTestData = () => {
@@ -62,15 +76,25 @@ export default function FormCertificationPage() {
         {teachingCertificatesArray.map((data, index) => (
           <TeachingCertificateDivs
             key={`certificateIndexKey${index}`}
-            parantData={data}
+            parentData={data}
             handleParentData={handleNewDataChanges}
             handleImageData={handleImageFileUploadData}
             index={index}
+            handleCertificationDeleltion={handleCertificationDeleltion}
           />
         ))}
       </div>
       <div>
-        <button>Add another certificate</button>{" "}
+        <button
+          onClick={() =>
+            setteachingCertificatesArray((prevState) => [
+              ...prevState,
+              initialData,
+            ])
+          }
+        >
+          Add another certificate
+        </button>
       </div>
       <div>
         <p>I don't have any teaching certificate yet</p>
@@ -83,94 +107,148 @@ export default function FormCertificationPage() {
 }
 
 function TeachingCertificateDivs({
-  parantData,
+  parentData,
   handleParentData,
   handleImageData,
+  handleCertificationDeleltion,
   index,
 }) {
+  // setting for study year end
+  const dateArray = ["2000", "2001", "2002", "2003"];
+  const [studyYearEndArray, setstudyYearEndArray] = useState(dateArray);
+
+  const handleStudyYearStartData = (e) => {
+    const newData = [...dateArray];
+
+    const dataIndex = newData.indexOf(e.target.value);
+    // console.log(newData.splice(dataIndex));
+    setstudyYearEndArray(newData.splice(dataIndex));
+    handleParentData(e, index);
+  };
+
   return (
     <div className="teachingCertificateIndividualDiv">
-      <div className="flexDiv">
-        <label htmlFor="randomId:subjecttaught5293000">Subject</label>
-        <select
-          name="subjectName"
-          // value={mainDataContainer.subjectTaught}
-          // onChange={handleInputValueChange}
-          id="randomId:subjecttaught5293000"
-        >
-          <option value=""></option>
-          <option value="python">Python</option>
-          <option value="scala">Scala</option>
-          <option value="java">Java</option>
-          <option value="javascript">Javascript</option>
-        </select>
-        {
-          <p className="validationError">
-            {/* {mainValidationError.subjectTaught} */}
-          </p>
-        }
-      </div>
-      <div className="flexDiv">
-        <label htmlFor="radnomId:opi395478">Certificate</label>
-        <input type="text" name="certificateName" />
-        <p className="validationError">Error</p>
-      </div>
-      <div className="flexDiv">
-        <label htmlFor="radnomId:opi395478" name="certificateDescription">
-          Description
-        </label>
-        <input type="text" />
-        <p className="validationError">Error</p>
-      </div>
-      <div className="flexDiv">
-        <label htmlFor="radnomId:opi395478" name="certificateIssuer">
-          Issued by
-        </label>
-        <input type="text" />
-        <p className="validationError">Error</p>
-      </div>
-      <div className="selectDivEducationPage">
-        <label>Years of study</label>
-        <div>
-          <select placeholder="Select" name="startStudyYear">
-            <option value="2000">2000</option>
-            <option value="2001">2001</option>
-            <option value="2002">2002</option>
+      <button onClick={() => handleCertificationDeleltion(index)}>
+        Delete
+      </button>
+      <div>
+        <div className="flexDiv">
+          <label htmlFor="randomId:subjecttaught5293000">Subject</label>
+          <select
+            name="subjectName"
+            value={parentData.subjectName}
+            onChange={(e) => handleParentData(e, index)}
+            id="randomId:subjecttaught5293000"
+          >
+            <option value=""></option>
+            <option value="python">Python</option>
+            <option value="scala">Scala</option>
+            <option value="java">Java</option>
+            <option value="javascript">Javascript</option>
           </select>
-          <span className="dashAdderSpan"></span>
-          <select name="endStudyYear">
-            <option value="2000">2000</option>
-            <option value="2001">2001</option>
-            <option value="2002">2002</option>
-          </select>
+          {parentData.subjectNameError && (
+            <p className="validationError">{parentData.subjectNameError}</p>
+          )}
         </div>
-        <p className="validationError">Error</p>
-      </div>
-      <div className="certificateImageAdditionDiv">
-        <div className="certificateInfoDiv">
-          <div className="certificateInfoImageSvgDiv">image</div>
-          <div>
-            <p className="bold">Get a "Certificate Verified" badge</p>
-            <p>
-              upload your certificate to increase the credibility of your
-              profile
+        <div className="flexDiv">
+          <label htmlFor="radnomId:opi395478">Certificate</label>
+          <input
+            type="text"
+            value={parentData.certificateName}
+            name="certificateName"
+            onChange={(e) => handleParentData(e, index)}
+          />
+          {parentData.certificateNameError && (
+            <p className="validationError">{parentData.certificateNameError}</p>
+          )}
+        </div>
+        <div className="flexDiv">
+          <label htmlFor="radnomId:opi395478">Description</label>
+          <input
+            type="text"
+            value={parentData.certificateDescription}
+            name="certificateDescription"
+            onChange={(e) => handleParentData(e, index)}
+          />
+          {parentData.certificateDescriptionError && (
+            <p className="validationError">
+              {parentData.certificateDescriptionError}
             </p>
-          </div>
+          )}
         </div>
-        <div className="certificateUploadDiv">
+        <div className="flexDiv">
+          <label htmlFor="radnomId:opi395478">Issued by</label>
+          <input
+            type="text"
+            name="certificateIssuer"
+            value={parentData.certificateIssuer}
+            onChange={(e) => handleParentData(e, index)}
+          />
+          {parentData.certificateIssuerError && (
+            <p className="validationError">
+              {parentData.certificateIssuerError}
+            </p>
+          )}
+        </div>
+        <div className="selectDivEducationPage">
+          <label>Years of study</label>
           <div>
-            <div>
-              <label htmlFor="randomId:fityu419278">Upload</label>
-              <input
-                type="file"
-                id="randomId:fityu419278"
-                className="noDisplay"
-              />
-            </div>
-            <span>JPG or PNG format. maximum size of 20MB</span>
+            <select
+              placeholder="Select"
+              name="startStudyYear"
+              value={parentData.startStudyYear}
+              onChange={handleStudyYearStartData}
+            >
+              {dateArray.map((element, index) => (
+                <option key={`yearStartArrayIndex${index}`} value={element}>
+                  {element}
+                </option>
+              ))}
+            </select>
+            <span className="dashAdderSpan"></span>
+            <select
+              name="endStudyYear"
+              value={parentData.endStudyYear}
+              onChange={(e) => handleParentData(e, index)}
+              disabled={parentData.startStudyYear ? false : true}
+            >
+              {studyYearEndArray.map((element, index) => (
+                <option key={`yearEndArrayIndex${index}`} value={element}>
+                  {element}
+                </option>
+              ))}
+            </select>
           </div>
-          <div className="certificateImageDiv">
-            <img src="" alt="image go here" />
+          {parentData.studyYearError && (
+            <p className="validationError">{parentData.studyYearError}</p>
+          )}
+        </div>
+        <div className="certificateImageAdditionDiv">
+          <div className="certificateInfoDiv">
+            <div className="certificateInfoImageSvgDiv">image</div>
+            <div>
+              <p className="bold">Get a "Certificate Verified" badge</p>
+              <p>
+                upload your certificate to increase the credibility of your
+                profile
+              </p>
+            </div>
+          </div>
+          <div className="certificateUploadDiv">
+            <div>
+              <div>
+                <label htmlFor="randomId:fityu419278">Upload</label>
+                <input
+                  type="file"
+                  id="randomId:fityu419278"
+                  className="noDisplay"
+                />
+              </div>
+              <span>JPG or PNG format. maximum size of 20MB</span>
+            </div>
+            <div className="certificateImageDiv">
+              <img src="" alt="image go here" />
+            </div>
           </div>
         </div>
       </div>
